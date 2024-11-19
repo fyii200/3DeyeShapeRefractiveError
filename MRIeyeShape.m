f20773b2136252270dbdc05c115f0168e185a0e9
Fabian Yii 俞思良 <72454128+fyii200@users.noreply.github.com>
2024-11-19 20:34:20 +0000

    Update MRIeyeShape.m

--- MRIeyeShape.m
+++ MRIeyeShape.m
@@ -8,9 +8,9 @@ homeDir = "";
 addpath(fullfile("matlabHelperFunctions", "imEllipsoid"));
 
 %%%%%%% Read segmentation info
-segInfo  = load(fullfile(homeDir, "imageOutputs", "UKB", "MRIsegmentation", "groundTruthMed.mat"));
-segPaths = segInfo.gTruthMed.LabelData;
-segPaths = segPaths(segPaths ~= "");
+segInfo    = load(fullfile(homeDir, "imageOutputs", "UKB", "MRIsegmentation", "groundTruthMed.mat"));
+segPaths   = segInfo.gTruthMed.LabelData;
+segPaths   = segPaths(segPaths ~= "");
 % RE pixels are labelled as "1", LE pixels are labelled "2"
 labelIDs   = segInfo.gTruthMed.LabelDefinitions.PixelLabelID;
 labelNames = segInfo.gTruthMed.LabelDefinitions.Name;
@@ -33,8 +33,8 @@ for i=1:length(segPaths)
         RE = data==labelIDs(1); 
         LE = data==labelIDs(2); 
     elseif ismember(labelNames(1), "LE") && ismember(labelNames(2), "RE")
-       LE = data==labelIDs(1); 
-       RE = data==labelIDs(2); 
+        LE = data==labelIDs(1); 
+        RE = data==labelIDs(2); 
     else
        throw(exception);
     end
@@ -42,14 +42,14 @@ for i=1:length(segPaths)
    %%%%%%% If RIGHT eye is available and eligible for analysis
    if length(unique(RE))>1
        % volshow(RE, RenderingStyle="GradientOpacity"); % show RE
-       volumeRE = nnz(RE)*mask.VoxelSpacing(1)*mask.VoxelSpacing(2)*mask.VoxelSpacing(3);
+       volumeRE                   = nnz(RE)*mask.VoxelSpacing(1)*mask.VoxelSpacing(2)*mask.VoxelSpacing(3);
        %%% Fit ellipsoid (https://uk.mathworks.com/matlabcentral/fileexchange/34104-image-ellipsoid-3d)
        %%% elliRE output: [XC, YC, ZC, A, B, C, PHI, THETA, PSI], where
        %%% [XC YC ZC] are the centroid coordinates [AL, width, height]; 
        %%% [A B C] are semidiameter of parameters A, B and C;
        %%% [PHI THETA PSI] are euler angles [z, y, x axis rotation sequence]
        [elliRE, rotationMatrixRE] = imEquivalentEllipsoid(RE); % RE data
-       REparamAxes = findAxes(rotationMatrixRE, 1);
+       REparamAxes                = findAxes(rotationMatrixRE, 1);
        
        %%% Visualise fitted ellipsoid
        % figure; hold on; 
@@ -71,9 +71,9 @@ for i=1:length(segPaths)
        %%% REparamAxes tells us which axis (i.e. axial length, width, height) 
        %%% parameters A, B and C correspond to in RE 
        [REcX, REcY, REcZ, A, B, C, REphi, REtheta, REpsi] = struct('x', num2cell(elliRE)).x;
-       params  = [A B C];
-       REsemiAL = params(find(strcmp(REparamAxes,"AL")));
-       REsemiWidth = params(find(strcmp(REparamAxes,"width")));
+       params       = [A B C];
+       REsemiAL     = params(find(strcmp(REparamAxes,"AL")));
+       REsemiWidth  = params(find(strcmp(REparamAxes,"width")));
        REsemiHeight = params(find(strcmp(REparamAxes,"height")));
        % Scale dimensional metrics (i.e. semidiameters) by their corresponding
        % pixel resolution (mm/pixel)
@@ -81,14 +81,14 @@ for i=1:length(segPaths)
        REsemiWidth  = REsemiWidth*mask.VoxelSpacing(2);
        REsemiHeight = REsemiHeight*mask.VoxelSpacing(3);
        % Asphericity: oblate if positive, circle if zero, prolate if negative
-       qREhor = ((REsemiWidth^2) / (REsemiAL^2)) - 1;  % horizontal oblateness
-       qREver = ((REsemiHeight^2) / (REsemiAL^2)) - 1; % vertical oblateness
+       qREhor       = ((REsemiWidth^2) / (REsemiAL^2)) - 1;  % horizontal oblateness
+       qREver       = ((REsemiHeight^2) / (REsemiAL^2)) - 1; % vertical oblateness
        % Vertex curvature
-       curveREhor = REsemiAL / (REsemiWidth^2);
-       curveREver = REsemiAL / (REsemiHeight^2); 
+       curveREhor   = REsemiAL / (REsemiWidth^2);
+       curveREver   = REsemiAL / (REsemiHeight^2); 
        % Vertex radius of curvature (mm)
-       radiusREhor = 1/curveREhor; 
-       radiusREver = 1/curveREver;
+       radiusREhor   = 1/curveREhor; 
+       radiusREver   = 1/curveREver;
        %%% Save results
        df(df.id==ID & df.eye=="RE", "volume")          = {volumeRE};
        df(df.id==ID & df.eye=="RE", "cX")              = {REcX};
@@ -108,9 +108,9 @@ for i=1:length(segPaths)
    %%%%%%% If LEFT eye is available and eligible for analysis
    if length(unique(LE))>1
        % volshow(LE, RenderingStyle="GradientOpacity"); % show LE
-       volumeLE = nnz(LE)*mask.VoxelSpacing(1)*mask.VoxelSpacing(2)*mask.VoxelSpacing(3);
+       volumeLE                   = nnz(LE)*mask.VoxelSpacing(1)*mask.VoxelSpacing(2)*mask.VoxelSpacing(3);
        [elliLE, rotationMatrixLE] = imEquivalentEllipsoid(LE); % LE data
-       LEparamAxes = findAxes(rotationMatrixLE, 1);
+       LEparamAxes                = findAxes(rotationMatrixLE, 1);
        
        %%% Visualise fitted ellipsoid
        % figure; hold on; 
@@ -127,19 +127,19 @@ for i=1:length(segPaths)
        % axis off;
        
        [LEcX, LEcY, LEcZ, A, B, C, LEphi, LEtheta, LEpsi] = struct('x', num2cell(elliLE)).x;
-       params  = [A B C];
-       LEsemiAL = params(find(strcmp(LEparamAxes,"AL")));
-       LEsemiWidth = params(find(strcmp(LEparamAxes,"width")));
+       params       = [A B C];
+       LEsemiAL     = params(find(strcmp(LEparamAxes,"AL")));
+       LEsemiWidth  = params(find(strcmp(LEparamAxes,"width")));
        LEsemiHeight = params(find(strcmp(LEparamAxes,"height")));
-       LEsemiAL = LEsemiAL*mask.VoxelSpacing(1);
-       LEsemiWidth = LEsemiWidth*mask.VoxelSpacing(2);
+       LEsemiAL     = LEsemiAL*mask.VoxelSpacing(1);
+       LEsemiWidth  = LEsemiWidth*mask.VoxelSpacing(2);
        LEsemiHeight = LEsemiHeight*mask.VoxelSpacing(3);
-       qLEhor = ((LEsemiWidth^2) / (LEsemiAL^2)) - 1;  
-       qLEver = ((LEsemiHeight^2) / (LEsemiAL^2)) - 1; 
-       curveLEhor = LEsemiAL / (LEsemiWidth^2);
-       curveLEver = LEsemiAL / (LEsemiHeight^2); 
-       radiusLEhor = 1/curveLEhor; 
-       radiusLEver = 1/curveLEver;
+       qLEhor       = ((LEsemiWidth^2) / (LEsemiAL^2)) - 1;  
+       qLEver       = ((LEsemiHeight^2) / (LEsemiAL^2)) - 1; 
+       curveLEhor   = LEsemiAL / (LEsemiWidth^2);
+       curveLEver   = LEsemiAL / (LEsemiHeight^2); 
+       radiusLEhor  = 1/curveLEhor; 
+       radiusLEver  = 1/curveLEver;
        df(df.id==ID & df.eye=="LE", "volume")          = {volumeLE};
        df(df.id==ID & df.eye=="LE", "cX")              = {LEcX};
        df(df.id==ID & df.eye=="LE", "cY")              = {LEcY};
